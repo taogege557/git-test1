551f400d7e2d6e1df4245744b978923ef7af908a
tom <tanteng@gmail.com>
2021-08-13 19:43:55 +0800

    idea git test two

--- src/main/java/com/wjt/controller/UserController.java
+++ src/main/java/com/wjt/controller/UserController.java
@@ -3,5 +3,6 @@ package com.wjt.controller;
 public class UserController {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello Git2");
     }
 }
