f9f8fe74ccd007f46b2cb6b6f0e42e7b554e2d5f
tom <tanteng@gmail.com>
2021-08-14 11:18:04 +0800

    push test

--- src/main/java/com/wjt/controller/UserController.java
+++ src/main/java/com/wjt/controller/UserController.java
@@ -4,6 +4,7 @@ public class UserController {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hot_fix test");
+        System.out.println("push test");
 
     }
 }
